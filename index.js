76c0cd1368e322d102bb10713149aeff9dd97662
ariful4082 <ariful4082@gmail.com>
2022-02-17 22:08:09 +0600

    creative forEach function customization

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
 /**
- * @Title : Sort Array of Object
+ * @Title : forEach
  */
 
-// const arr = [67, 34, 21, 45, 6, 29, 38, 55, 76, 10];
+const arr = [1, 2, 3, 4, 5];
 
-// arr.sort((a, b) => a- b);
-
-// console.log(arr);
-
-const people = [
-  { name: "Ariful Islam", age: 28 },
-  { name: "Abdullah Al Zarif", age: 2 },
-  { name: "Asadullah Al Labib", age: 0.25 },
-  { name: "Johirul Islam", age: 25 },
-  { name: "Azizul Islam", age: 23 },
-  { name: "Asia Khatun", age: 20 },
-];
-
-// people.sort(function(a, b){
-//   return b.age - a.age
+// arr.forEach((value, index, array) => {
+//   console.log(index + ". Element is = " + value + ' Array '+ array);
 // });
 
-people.sort((a, b) => {
-  // return a.name - b.name;
-
-  if (a.name > b.name) {
-    return 1;
-  } else if (b.name > a.name) {
-    return -1;
-  } else {
-    return 0;
+function loop(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i], i, arr);
   }
-});
-console.log(people);
+}
+
+
+const result = loop(arr, function(v, idx, array){
+  console.log(idx + ". Element is = " + v + ' Array '+ array);
+})
